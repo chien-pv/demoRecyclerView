f9e5bd2c8ceb7297dd7431f2c904b3a0bda9b298
Pham Van Chien <phamvanbk11@gmail.com>
2017-11-16 18:59:32 +0700

    demoRecyclerView

---
+++ app/src/main/java/com/example/framgia/demorecyclerview/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.framgia.demorecyclerview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import java.util.ArrayList;
+import java.util.List;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView mRecyclerView;
+    RecyclerViewAdapter mRcvAdapter;
+    List<String> data;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        data = new ArrayList<>();
+        data.add("Nguyễn Minh Hưng");
+        data.add("Hoàng Minh Lợi");
+        data.add("Nguyễn Duy Bảo");
+        data.add("Nguyễn Ngọc Doanh");
+        data.add("Nguyễn Phạm Thế Hà");
+        data.add("Trần Anh Đức");
+        data.add("Trần Minh Hải");
+        mRcvAdapter = new RecyclerViewAdapter(this, data);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setAdapter(mRcvAdapter);
+
+        mRcvAdapter.setOnItemClickedListener(new RecyclerViewAdapter.OnItemClickedListener() {
+            @Override
+            public void onItemClick(String username) {
+                Toast.makeText(MainActivity.this, username, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+}
